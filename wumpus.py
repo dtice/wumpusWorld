237953518c9c3674e85078e36c812677b8d966f1
Dillon Tice <dtice@localhost.localdomain>
2018-11-27 20:01:35 -0700

    playing with wumpus (no longer working)

--- wumpus.py
+++ wumpus.py
@@ -5,6 +5,16 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from logic import *
 from agents import *
 
+agt = KB_AgentProgram(WumpusKB(6))
+env = WumpusEnvironment(agt)
 
-env = WumpusEnvironment(KB_AgentProgram(WumpusKB(6)))
-env.get_world()
+# main loop for agent performing actions
+while not env.is_done():
+    # print board
+    for something in env.get_world(show_walls=False):
+        print(something)
+    # agent takes in percepts
+    env.execute_action(env.myExplorer, env.percept(agt))
+    # asks knowledge base what to do
+    # execute action
+    # repeat
